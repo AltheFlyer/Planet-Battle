99c94c468fedd542ad3878cf1b9ab986da332852
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2018-11-24 22:56:31 -0500

    2nd phase Venus

--- core/src/com/battle/planet/Venus.java
+++ core/src/com/battle/planet/Venus.java
@@ -3,6 +3,7 @@ package com.battle.planet;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
 public class Venus extends Enemy {
@@ -13,10 +14,15 @@ public class Venus extends Enemy {
     final float SPAWN_MAX_COOLDOWN = 3f;
     int spawned = 0;
 
+    float aimCooldown;
+    final float AIM_MAX_COOLDOWN = 3f;
+
     public Venus(float x, float y) {
         super(x, y, 70, 80, 500);
 
         spawnCooldown = SPAWN_MAX_COOLDOWN;
+        aimCooldown = AIM_MAX_COOLDOWN;
+
         phaseMarkers.add(400);
     }
 
@@ -36,6 +42,33 @@ public class Venus extends Enemy {
                 canSpawn = true;
             }
         }
+        if (phase == 2) {
+            //Try to get away from player if distance < 200
+            if (Vector2.dst2(x, y, hitbox.x, hitbox.y) <= 40000) {
+                float angle = MathUtils.atan2(y - hitbox.y, x - hitbox.x);
+                hitbox.x -= MathUtils.cos(angle) * 200 * frame;
+                hitbox.y -= MathUtils.sin(angle) * 200 * frame;
+            //Try to get closer to player if distance > 300
+            } else if (Vector2.dst2(x, y, hitbox.x, hitbox.y) >= 90000) {
+                float angle = MathUtils.atan2(y - hitbox.y, x - hitbox.x);
+                hitbox.x += MathUtils.cos(angle) * 200 * frame;
+                hitbox.y += MathUtils.sin(angle) * 200 * frame;
+            }
+
+            //Prevent offscreen
+            if (hitbox.x < 0 + hitbox.radius) {
+                hitbox.x = 0 + hitbox.radius;
+            } else if (hitbox.x > 600 - hitbox.radius) {
+                hitbox.x = 600 - hitbox.radius;
+            }
+            if (hitbox.y < 0 + hitbox.radius) {
+                hitbox.y = 0 + hitbox.radius;
+            } else if (hitbox.y > 600 - hitbox.radius) {
+                hitbox.y = 600 - hitbox.radius;
+            }
+
+
+        }
         if (phase == 0) {
             //Create orbital ring
             for (int i = 0; i < 72; ++i) {
@@ -45,7 +78,11 @@ public class Venus extends Enemy {
             phase = 1;
         }
         if (phase == 1 && health <= 400) {
-            phase = 2;
+            phase = -1;
+        }
+        if (phase == -1) {
+            canSpawn = true;
+
         }
         return bullets;
     }
@@ -53,25 +90,36 @@ public class Venus extends Enemy {
     @Override
     public Array<Enemy> spawn(float x, float y, float frame) {
         Array<Enemy> enemies = new Array<Enemy>();
-        int rand = MathUtils.random(0, 3);
-        //Spawn from random sides
-        if (rand == 0) {
-            enemies.add(new AcidCloud(MathUtils.random(0, 600), 670, this));
-        } else if (rand == 1) {
-            enemies.add(new AcidCloud(MathUtils.random(0, 600), -70, this));
-        } else if (rand == 2) {
-            enemies.add(new AcidCloud(670, MathUtils.random(0, 600), this));
-        } else if (rand == 3) {
-            enemies.add(new AcidCloud(-70, MathUtils.random(0, 600), this));
+        if (phase == 1) {
+            int rand = MathUtils.random(0, 3);
+            //Spawn from random sides
+            if (rand == 0) {
+                enemies.add(new AcidCloud(MathUtils.random(0, 600), 670, this));
+            } else if (rand == 1) {
+                enemies.add(new AcidCloud(MathUtils.random(0, 600), -70, this));
+            } else if (rand == 2) {
+                enemies.add(new AcidCloud(670, MathUtils.random(0, 600), this));
+            } else if (rand == 3) {
+                enemies.add(new AcidCloud(-70, MathUtils.random(0, 600), this));
+            }
+            canSpawn = false;
+            spawned += 1;
         }
-        canSpawn = false;
-        spawned += 1;
+        if (phase == -1) {
+            for (int i = 0; i < 8; ++i) {
+                enemies.add(new VenusShield(130, 0.785398f * i, 0.785398f, this));
+                enemies.add(new VenusSniper(100, 0.785398f * i, 0.785398f, this));
+            }
+            spawned += 16;
+            canSpawn = false;
+            phase = 2;
+        }
         return enemies;
     }
 
     @Override
     public Array<Projectile> collide(Array<Projectile> projectiles) {
-        if (spawned == 0) {
+        if (spawned == 0 || phase != 1) {
             for (Projectile p : projectiles) {
                 if (!p.isDestroyed && this.hitbox.contains(p.hitbox.x, p.hitbox.y)) {
                     p.isDestroyed = true;
---
+++ core/src/com/battle/planet/VenusSniper.java
@@ -0,0 +1,78 @@
+package com.battle.planet;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
+
+public class VenusSniper extends Enemy {
+
+    final Venus venus;
+    float angularPosition; // Relative to venus
+    float angularVelocity;
+    float distance; //Relative to venus
+
+    float aimCooldown;
+    final float MAX_AIM_COOLDOWN = 3f;
+
+    public VenusSniper(float r, float theta, float omega, Venus v) {
+        super(0, 0, 20, 20, 7);
+        venus = v;
+
+        distance = r;
+        angularPosition = theta;
+        angularVelocity = omega;
+
+        aimCooldown = MAX_AIM_COOLDOWN;
+    }
+
+    @Override
+    public void drawBody(ShapeRenderer r) {
+        r.setColor(Color.GOLDENROD);
+        r.circle(hitbox.x, hitbox.y, hitbox.radius);
+    }
+
+    @Override
+    public void drawObjects(float x, float y, ShapeRenderer r) {
+        if (aimCooldown >= 0.5f) {
+            r.setColor(Color.ORANGE);
+        } else {
+            r.setColor(Color.YELLOW);
+        }
+        float angle = MathUtils.atan2(y - hitbox.y, x - hitbox.x);
+        r.rectLine(hitbox.x, hitbox.y, hitbox.x + 800 * MathUtils.cos(angle), hitbox.y + 800 * MathUtils.sin(angle), 3);
+    }
+
+    @Override
+    public Array<Projectile> attack(float x, float y, float frame) {
+        bullets.clear();
+        aimCooldown -= frame;
+        if (aimCooldown <= 0) {
+            float angle = MathUtils.atan2(y - hitbox.y, x - hitbox.x);
+            for (int i = -8; i < 8; ++i) {
+                bullets.add(new BasicProjectile(hitbox.x + i * 2 * MathUtils.cos(angle), hitbox.y + i * 2 * MathUtils.sin(angle), MathUtils.cos(angle) * 800, MathUtils.sin(angle) * 800));
+            }
+            aimCooldown = MAX_AIM_COOLDOWN;
+        }
+
+        angularPosition += angularVelocity * frame;
+        hitbox.x = venus.hitbox.x + MathUtils.cos(angularPosition) * distance;
+        hitbox.y = venus.hitbox.y + MathUtils.sin(angularPosition) * distance;
+
+        return bullets;
+    }
+
+    @Override
+    public Array<Projectile> collide(Array<Projectile> projectiles) {
+        for (Projectile p: projectiles) {
+            if (!p.isDestroyed && this.hitbox.contains(p.hitbox.x, p.hitbox.y)) {
+                p.isDestroyed = true;
+                this.health -= 1;
+            }
+        }
+        if (health <= 0) {
+            venus.spawned -= 1;
+        }
+        return projectiles;
+    }
+}
