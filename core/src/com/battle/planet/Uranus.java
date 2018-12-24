9604c8c4ce8f092c1704c21a704000009839d50f
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2018-12-24 12:41:52 -0500

    OOP Abilities

--- core/src/com/battle/planet/Uranus.java
+++ core/src/com/battle/planet/Uranus.java
@@ -9,18 +9,9 @@ public class Uranus extends Enemy {
 
     int phase = 0;
 
-    int ability = 0;
-    float abilityTimer = 0f;
-    float abilityCooldown = 0f;
-    float targetX = 0, targetY = 0;
-    float modifier = 0;
+    int chosenAbility = 0;
 
-    final float[] ABILITY_TIMERS = {
-            0, 10, 4
-    };
-    final float[] ABILITY_COOLDOWNS = {
-            0, 0.5f, 0.05f
-    };
+    Ability ability;
 
     float teleportCooldown;
     final float TELE_MAX_COOLDOWN = 1f;
@@ -42,6 +33,13 @@ public class Uranus extends Enemy {
     }
 
     @Override
+    public void drawObjects(final Player player, ShapeRenderer r) {
+        r.setColor(Color.ORANGE);
+        //TODO Make the offscreen indicator better
+        r.rectLine(player.hitboxCenter.x, player.hitboxCenter.y, hitbox.x, hitbox.y, 2);
+    }
+
+    @Override
     public Array<Projectile> attack(Player player, float frame) {
         bullets.clear();
         /*
@@ -56,89 +54,136 @@ public class Uranus extends Enemy {
             bullets.add(new BasicProjectile(hitbox.x, hitbox.y, MathUtils.cos(angle) * 200, MathUtils.sin(angle) * 200));
         }
         */
-        if (ability == 0) {
-            ability = MathUtils.random(1, 2);
-            if (ability == 1) {
-                abilityTimer = ABILITY_TIMERS[1];
-                abilityCooldown = 0;
-                targetX = player.hitboxCenter.x;
-                targetY = player.hitboxCenter.y;
-                modifier = 0;
-                System.out.println("ENTERING ABILITY 1");
-                for (int i = 0; i < 360; ++i) {
-                    float angle = MathUtils.degreesToRadians * i;
-                    bullets.add(new TimeProjectile(targetX + MathUtils.cos(angle) * 400, targetY + MathUtils.sin(angle) * 400, 10));
-                }
-                do {
-                    float angle = MathUtils.random(0, 360) * MathUtils.degreesToRadians;
-                    hitbox.x = targetX + MathUtils.cos(angle) * (400 + hitbox.radius);
-                    hitbox.y = targetY + MathUtils.sin(angle) * (400 + hitbox.radius);
-                } while (hitbox.x > 1100 && hitbox.y > 1100 && hitbox.x < 100 && hitbox.y < 100);
-            } else if (ability == 2) {
-                abilityTimer = ABILITY_TIMERS[2];
-                abilityCooldown = ABILITY_COOLDOWNS[2];
-                targetX = player.hitboxCenter.x;
-                targetY = player.hitboxCenter.y;
-                System.out.println("ENTERING ABILITY 2");
-                for (int i = 0; i < 360; ++i) {
-                    float angle = MathUtils.degreesToRadians * i;
-                    bullets.add(new TimeProjectile(targetX + MathUtils.cos(angle) * 400, targetY + MathUtils.sin(angle) * 400, 4));
-                }
-                do {
-                    float angle = MathUtils.random(0, 360) * MathUtils.degreesToRadians;
-                    hitbox.x = targetX + MathUtils.cos(angle) * (400 + hitbox.radius);
-                    hitbox.y = targetY + MathUtils.sin(angle) * (400 + hitbox.radius);
-                } while (hitbox.x > 1100 && hitbox.y > 1100 && hitbox.x < 100 && hitbox.y < 100);
+        if (chosenAbility == 0) {
+            chosenAbility = MathUtils.random(1, 2);
+            if (chosenAbility == 1) {
+                ability = new SwirlAbility(player, this);
+            }
+            if (chosenAbility == 2) {
+                ability = new IndirectAbility(player, this);
             }
         }
 
-        if (ability == 1) {
-            abilityCooldown -= frame;
-            if (abilityCooldown <= 0 && abilityTimer > 3) {
-                abilityCooldown = ABILITY_COOLDOWNS[1];
+        if (chosenAbility != 0) {
+            ability.run(frame);
+            ability.timer -= frame;
+        }
+
+        if (ability.timer <= 0) {
+            chosenAbility = 0;
+            System.out.println("ENTERING ABILITY 0");
+        }
+
+        return bullets;
+    }
+
+    class Ability {
+        float timer;
+        final Uranus uranus;
+        final Player player;
+
+        public Ability(final Player p, final Uranus u) {
+            uranus = u;
+            player = p;
+        }
+
+        public void run(float frame) {
+
+        }
+    }
+
+    class SwirlAbility extends Ability {
+
+        float targetX, targetY, modifier;
+        float cooldown;
+        final float MAX_COOLDOWN = 0.5f;
+
+        public SwirlAbility(final Player p, final Uranus u) {
+            super(p, u);
+            timer = 10;
+            targetX = p.hitboxCenter.x;
+            targetY = p.hitboxCenter.y;
+            modifier = 0;
+            System.out.println("ENTERING ABILITY 1");
+            for (int i = 0; i < 360; ++i) {
+                float angle = MathUtils.degreesToRadians * i;
+                uranus.bullets.add(new TimeProjectile(targetX + MathUtils.cos(angle) * 400, targetY + MathUtils.sin(angle) * 400, 10));
+            }
+            do {
+                float angle = MathUtils.random(0, 360) * MathUtils.degreesToRadians;
+                uranus.hitbox.x = targetX + MathUtils.cos(angle) * (400 + uranus.hitbox.radius);
+                uranus.hitbox.y = targetY + MathUtils.sin(angle) * (400 + uranus.hitbox.radius);
+            } while (uranus.hitbox.x > 1100 || uranus.hitbox.y > 1100 || uranus.hitbox.x < 100 || uranus.hitbox.y < 100);
+        }
+
+        @Override
+        public void run(float frame) {
+            cooldown -= frame;
+            if (cooldown <= 0 && timer > 3) {
+                cooldown = MAX_COOLDOWN;
                 for (int i = 0; i < 12; ++i) {
                     float angle = (i / 12.0f) * MathUtils.PI2 + modifier;
-                    bullets.add(new BasicProjectile(targetX + MathUtils.cos(angle) * 400, targetY + MathUtils.sin(angle) * 400, -MathUtils.cos(angle) * 150, -MathUtils.sin(angle) * 150));
+                    uranus.bullets.add(new BasicProjectile(targetX + MathUtils.cos(angle) * 400, targetY + MathUtils.sin(angle) * 400, -MathUtils.cos(angle) * 150, -MathUtils.sin(angle) * 150));
                 }
                 modifier += MathUtils.PI / 48;
             }
         }
+    }
 
-        if (ability == 2) {
-            abilityCooldown -= frame;
-            if (abilityCooldown <= 0) {
-                abilityCooldown = ABILITY_COOLDOWNS[2];
+    class IndirectAbility extends Ability{
+
+        float targetX, targetY, cooldown;
+        final float MAX_COOLDOWN = 0.05f;
+
+        public IndirectAbility(final Player p, final Uranus u) {
+            super(p, u);
+            timer = 4;
+            cooldown = 0;
+            targetX = player.hitboxCenter.x;
+            targetY = player.hitboxCenter.y;
+            System.out.println("ENTERING ABILITY 2");
+            for (int i = 0; i < 360; ++i) {
+                float angle = MathUtils.degreesToRadians * i;
+                uranus.bullets.add(new TimeProjectile(targetX + MathUtils.cos(angle) * 400, targetY + MathUtils.sin(angle) * 400, 4));
+            }
+            do {
+                float angle = MathUtils.random(0, 360) * MathUtils.degreesToRadians;
+                uranus.hitbox.x = targetX + MathUtils.cos(angle) * (400 + uranus.hitbox.radius);
+                uranus.hitbox.y = targetY + MathUtils.sin(angle) * (400 + uranus.hitbox.radius);
+            } while (uranus.hitbox.x > 1100 || uranus.hitbox.y > 1100 || uranus.hitbox.x < 100 || uranus.hitbox.y < 100);
+        }
+
+        @Override
+        public void run(float frame) {
+            cooldown -= frame;
+            if (cooldown <= 0) {
+                cooldown = MAX_COOLDOWN;
                 float angle = 0;
                 boolean canShoot = false;
-                if (abilityTimer > 3.0f) {
+                if (timer > 3.0f) {
                     angle = MathUtils.PI / 4.0f;
                     canShoot = true;
-                } else if (abilityTimer > 2.0f) {
+                } else if (timer > 2.0f) {
                     angle = (3 * MathUtils.PI) / 4.0f;
                     canShoot = true;
-                } else if (abilityTimer > 1.0f) {
+                } else if (timer > 1.0f) {
                     angle = (5 * MathUtils.PI) / 4.0f;
                     canShoot = true;
-                } else if (abilityTimer > 0.0f) {
+                } else if (timer > 0.0f) {
                     angle = (7 * MathUtils.PI) / 4.0f;
                     canShoot = true;
                 }
                 if (canShoot) {
                     float nAngle = angle + MathUtils.PI / 2;
                     float nAngle2 = angle - MathUtils.PI / 2;
-                    bullets.add(new BasicProjectile(targetX + 14.14f * MathUtils.cos(nAngle) + MathUtils.cos(angle) * 400, targetY + 14.14f * MathUtils.sin(nAngle) + MathUtils.sin(angle) * 400, -MathUtils.cos(angle) * 300, -MathUtils.sin(angle) * 300));
-                    bullets.add(new BasicProjectile(targetX + 14.14f * MathUtils.cos(nAngle2) + MathUtils.cos(angle) * 400, targetY + 14.14f * MathUtils.sin(nAngle2) + MathUtils.sin(angle) * 400, -MathUtils.cos(angle) * 300, -MathUtils.sin(angle) * 300));
+                    uranus.bullets.add(new BasicProjectile(targetX + 20 * MathUtils.cos(nAngle) + MathUtils.cos(angle) * 400, targetY + 20 * MathUtils.sin(nAngle) + MathUtils.sin(angle) * 400, -MathUtils.cos(angle) * 300, -MathUtils.sin(angle) * 300));
+                    uranus.bullets.add(new BasicProjectile(targetX + 20 * MathUtils.cos(nAngle2) + MathUtils.cos(angle) * 400, targetY + 20 * MathUtils.sin(nAngle2) + MathUtils.sin(angle) * 400, -MathUtils.cos(angle) * 300, -MathUtils.sin(angle) * 300));
                 }
             }
         }
-
-        //Tick ability time
-        abilityTimer -= frame;
-        if (abilityTimer <= 0) {
-            ability = 0;
-            System.out.println("ENTERING ABILITY 0");
-        }
-
-        return bullets;
     }
+
+
+
+
 }
