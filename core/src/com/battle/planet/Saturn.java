116084881d0665251c48bd06e743df455e6ab4ff
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2019-01-10 22:53:17 -0500

    +1 Moon Type

--- core/src/com/battle/planet/Saturn.java
+++ core/src/com/battle/planet/Saturn.java
@@ -3,6 +3,7 @@ package com.battle.planet;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
 public class Saturn extends Enemy {
@@ -21,7 +22,7 @@ public class Saturn extends Enemy {
     }
 
     @Override
-    public Array<Projectile> collide(Array<Projectile> projectiles) {
+    public Array<Projectile> attack(float frame) {
         if (phase == 0) {
             canSpawn = true;
         }
@@ -37,6 +38,7 @@ public class Saturn extends Enemy {
         Array<Enemy> enemies = new Array<Enemy>();
         if (phase == 0) {
             enemies.add(new SaturnMoonA(level, hitbox.x, hitbox.y + 300));
+            enemies.add(new SaturnMoonB(level, hitbox.x, hitbox.y + 300));
             canSpawn = false;
             phase = 1;
         }
@@ -48,9 +50,11 @@ public class Saturn extends Enemy {
 
         float cooldown = 0;
         float MAX_COOLDOWN = 1.0f;
+        Vector2 velocity;
 
         public SaturnMoonA(BattleLevel lev, float x, float y) {
             super(lev, x, y, 50, 60, 100);
+            velocity = new Vector2(120, 120);
         }
 
         @Override
@@ -62,13 +66,94 @@ public class Saturn extends Enemy {
         @Override
         public Array<Projectile> attack(float frame) {
             this.bullets.clear();
+            //Shoot
             cooldown -= frame;
 
             if (cooldown <= 0) {
                 createSpread(0, 10, MathUtils.PI * 2);
                 cooldown = MAX_COOLDOWN;
             }
+
+            //Move
+            this.hitbox.x += this.velocity.x * frame;
+            this.hitbox.y += this.velocity.y * frame;
+            if (this.hitbox.x - this.hitbox.radius < 0) {
+                this.hitbox.x = this.hitbox.radius;
+                this.velocity.x *= -1;
+            }
+            if (this.hitbox.x + this.hitbox.radius > level.LEVEL_WIDTH) {
+                this.hitbox.x = level.LEVEL_WIDTH - this.hitbox.radius;
+                this.velocity.x *= -1;
+            }
+            if (this.hitbox.y - this.hitbox.radius < 0) {
+                this.hitbox.y = this.hitbox.radius;
+                this.velocity.y *= -1;
+            }
+            if (this.hitbox.y + this.hitbox.radius > level.LEVEL_HEIGHT) {
+                this.hitbox.y = level.LEVEL_HEIGHT - this.hitbox.radius;
+                this.velocity.y *= -1;
+            }
             return this.bullets;
         }
     }
+
+    public class SaturnMoonB extends Enemy {
+
+        float cooldown = 0.25f;
+        float MINI_COOLDOWN = 0.25f;
+        float MAX_COOLDOWN = 1.0f;
+        float shots = 4;
+
+        Vector2 velocity;
+
+        public SaturnMoonB(BattleLevel lev, float x, float y) {
+            super(lev, x, y, 50, 60, 100);
+            velocity = new Vector2(-120, 120);
+        }
+
+        @Override
+        public void drawBody(ShapeRenderer r) {
+            r.setColor(Color.GRAY);
+            r.circle(hitbox.x, hitbox.y, hitbox.radius);
+        }
+
+        @Override
+        public Array<Projectile> attack(float frame) {
+            this.bullets.clear();
+            //Shoot
+            cooldown -= frame;
+
+            if (cooldown <= 0 && shots > 0) {
+                float angle = MathUtils.atan2(player.hitboxCenter.y - this.hitbox.y, player.hitboxCenter.x - this.hitbox.x);
+                this.bullets.add(new BasicProjectile(level, hitbox.x, hitbox.y, MathUtils.cos(angle) * 260, MathUtils.sin(angle) * 260));
+                cooldown = MINI_COOLDOWN;
+                --shots;
+            } else if (shots == 0) {
+                cooldown = MAX_COOLDOWN;
+                shots = 4;
+            }
+
+            //Move
+            this.hitbox.x += this.velocity.x * frame;
+            this.hitbox.y += this.velocity.y * frame;
+            if (this.hitbox.x - this.hitbox.radius < 0) {
+                this.hitbox.x = this.hitbox.radius;
+                this.velocity.x *= -1;
+            }
+            if (this.hitbox.x + this.hitbox.radius > level.LEVEL_WIDTH) {
+                this.hitbox.x = level.LEVEL_WIDTH - this.hitbox.radius;
+                this.velocity.x *= -1;
+            }
+            if (this.hitbox.y - this.hitbox.radius < 0) {
+                this.hitbox.y = this.hitbox.radius;
+                this.velocity.y *= -1;
+            }
+            if (this.hitbox.y + this.hitbox.radius > level.LEVEL_HEIGHT) {
+                this.hitbox.y = level.LEVEL_HEIGHT - this.hitbox.radius;
+                this.velocity.y *= -1;
+            }
+            return this.bullets;
+        }
+    }
+
 }
