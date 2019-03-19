a74996ee4e2bbb1bc6dae645e206cb3798504917
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2019-03-18 21:31:43 -0400

    Put your op mars edits here pls

---
+++ core/src/com/battle/planet/HardMars.java
@@ -0,0 +1,10 @@
+package com.battle.planet;
+
+public class HardMars extends Mars {
+
+    public HardMars(final BattleLevel lev, float x, float y) {
+        super(lev, x, y);
+    }
+
+    //Add your hardmode code here:
+}
